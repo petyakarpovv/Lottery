fc4c2d87ee322d2d73641d384fa580bb60b5ddf5
PeterBurrfot <arr.in.the.pw@gmail.com>
2023-11-12 00:12:18 +0300

    Lottery

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         List<Integer> lottery = new ArrayList<>();
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 0; i < 100; i++) {
             lottery.add(i);
         }
         Random random = new Random();
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 0; i < 100; i++) {
             int index = random.nextInt(100);
             int temp = lottery.get(index);
             lottery.set(index, lottery.get(i));
